08ca6b0ae876e8857e1802af0f5c1a7094d479a7
hathu03 <nguyenthuha2411vmh@gmail.com>
2021-09-16 21:32:48 +0700

    hien thi danh sach khach hang

---
+++ index.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        th, td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+    </style>
+</head>
+<body>
+<table>
+    <caption><h1>Danh sách khách hàng</h1></caption>
+    <thead>
+    <tr>
+        <th>STT</th>
+        <th>Tên</th>
+        <th>Ngày sinh</th>
+        <th>Địa chỉ</th>
+        <th>Ảnh</th>
+    </tr>
+    </thead>
+
+    <tbody>
+    <?php
+    $customerlist = array(
+        array("ten" => "Mai Văn Hoàn",
+            "ngaysinh" => "1983-08-20",
+            "diachi" => "Hà Nội",
+            "anh" => "images/img1.png"),
+        array("ten" => "Nguyễn Văn Nam",
+            "ngaysinh" => "1983-08-20",
+            "diachi" => "Bắc Giang",
+            "anh" => "images/img2.png"),
+        array("ten" => "Nguyễn Thái Hòa",
+            "ngaysinh" => "1983-08-21",
+            "diachi" => "Nam Định",
+            "anh" => "images/img3.png"),
+        array("ten" => "Trần Đăng Khoa",
+            "ngaysinh" => "1983-08-22",
+            "diachi" => "Hà Tây",
+            "anh" => "images/img4.png"),
+        array("ten" => "Nguyễn Đình Thi",
+            "ngaysinh" => "1983-08-17",
+            "diachi" => "Hà Nội",
+            "anh" => "images/img5.png")
+    );
+
+    foreach($customerlist as $key => $values){
+        echo "<tr>";
+        echo "<td>".$key."</td>";
+        echo "<td>".$values['ten']."</td>";
+        echo "<td>".$values['ngaysinh']."</td>";
+        echo "<td>".$values['diachi']."</td>";
+        echo "<td><image src ='".$values['anh']."' width = '60px' height ='60px'/></td>";
+        echo "</tr>";
+    }
+    ?>
+
+    </tbody>
+</table>
+</body>
+</html>
+
+
